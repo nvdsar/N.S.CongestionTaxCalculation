74396c18af02df84061bb6feb2e88e7fc01ccda0
Navid Sargheiny <nvdsar10133@gmail.com>
2023-09-15 19:15:34 +0330

    calculation refactor

--- Calculator/GothenburgTaxCalculator.cs
+++ Calculator/GothenburgTaxCalculator.cs
@@ -15,58 +15,20 @@ namespace Calculator
 
             dates = dates.OrderBy(d => d.Date).ThenBy(d => d.TimeOfDay).ToArray();
             var totalFee = 0;
-            var calculatedTimes = new List<DateTime>();
+            var taxCalculatedDates = new List<DateTime>();
             for (var i = 0; i <= dates.Length - 1; i++)
             {
+
                 var currentTime = dates[i];
-                var currentFee = GetTollFee(currentTime, vehicle);
-                if (i != 0)
-                {
-                    DateTime previousTime = dates[i - 1];
-                    var diffInMinute = new TimeSpan(currentTime.Ticks - previousTime.Ticks).TotalMilliseconds / 1000 / 60;
-                    if (Math.Abs(diffInMinute) <= 60)
-                    {
-                        if (totalFee > 0)
-                        {
-                            var previousFee = GetTollFee(previousTime, vehicle);
-                            totalFee -= previousFee;
-                            if (previousFee >= currentFee)
-                                currentFee = previousFee;
-                        }
-                    }
-                }
-                totalFee += currentFee;
-
-
+                if (taxCalculatedDates.Contains(currentTime))
+                    continue;
+                var within60MinutesEntries = dates.Where(x => IsWithin60Minutes(x, currentTime) && taxCalculatedDates.Contains(x) == false);
+                var maxToll = within60MinutesEntries.Max(x => GetTollFee(x, vehicle));
+                totalFee += maxToll;
+                taxCalculatedDates.AddRange(within60MinutesEntries);
             }
-            //foreach (DateTime date in dates)
-            //{
-            //    int nextFee = GetTollFee(date, vehicle);
-            //    int tempFee = GetTollFee(intervalStart, vehicle);
-
-            //    long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            //    long minutes = diffInMillies / 1000 / 60;
-
-            //    if (minutes <= 60)
-            //    {
-            //        if (totalFee > 0) totalFee -= tempFee;
-            //        if (nextFee >= tempFee) tempFee = nextFee;
-            //        totalFee += tempFee;
-            //    }
-            //    else
-            //    {
-            //        totalFee += nextFee;
-            //    }
-            //}
             if (totalFee > 60) totalFee = 60;
             return totalFee;
-        }
-        private bool IsWithin60Minutes(DateTime initialDateTime, DateTime secondaryDateTime)
-        {
-            var diffInMinute = new TimeSpan(initialDateTime.Ticks - secondaryDateTime.Ticks).TotalMilliseconds / 1000 / 60;
-            if (Math.Abs(diffInMinute) <= 60)
-                return true;
-            return false;
         }
         protected override bool IsTollFreeVehicle(IVehicle vehicle)
         {
@@ -99,6 +61,14 @@ namespace Calculator
             if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
             if (DateSystem.IsPublicHoliday(date, CountryCode.GB) || DateSystem.IsPublicHoliday(date.AddDays(1), CountryCode.GB)) return true;
             if (date.Month == (int)Month.July) return true;
+            return false;
+        }
+
+        private bool IsWithin60Minutes(DateTime initialDateTime, DateTime secondaryDateTime)
+        {
+            var diffInMinute = new TimeSpan(initialDateTime.Ticks - secondaryDateTime.Ticks).TotalMilliseconds / 1000 / 60;
+            if (Math.Abs(diffInMinute) <= 60)
+                return true;
             return false;
         }
     }
